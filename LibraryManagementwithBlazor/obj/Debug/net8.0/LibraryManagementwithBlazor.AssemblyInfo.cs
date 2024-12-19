72b3ff8ccaf9c4250254f66d0ca274df12db4b4c
EGV <elias.guerematchi.vrabl@gmail.com>
2024-12-19 15:07:35 +0100

    borrowing gui

--- LibraryManagementwithBlazor/obj/Debug/net8.0/LibraryManagementwithBlazor.AssemblyInfo.cs
+++ LibraryManagementwithBlazor/obj/Debug/net8.0/LibraryManagementwithBlazor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementwithBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ddc587eb0d2256a928040dffb43fc0e2f25c349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14d17a86a7b22c027bfdbef03c0ff321e3c20f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementwithBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementwithBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
